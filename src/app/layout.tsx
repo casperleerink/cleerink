b50329edb5018f992c405bce2accdfb66da5be54
Hooman Studio <development@hoomanstudio.com>
2023-02-05 19:10:48 -0800

    site placeholder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
-import './globals.css'
+import "./globals.css";
+import { Urbanist } from "@next/font/google";
+
+const urbanist = Urbanist({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-urbanist",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={urbanist.variable}>
       <head />
-      <body>{children}</body>
+      <body className="font-urbanist font-light bg-gray-900 text-gray-100 selection:bg-beige selection:text-gray-900">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      urbanist: ["var(--font-urbanist)", ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      white: "#fff",
+      black: "#000",
+      transparent: "transparent",
+      current: "currentColor",
+      gray: {
+        100: "#F3F3F3",
+        800: "#25292B",
+        900: "#1E1E1E",
+      },
+      beige: "#F0E7B5",
+    },
+    extend: {
+      keyframes: {
+        "slide-left": {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
